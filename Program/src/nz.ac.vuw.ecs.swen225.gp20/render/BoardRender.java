6dc10da29a924c8c28c7382777a563128bde730c
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2020-09-19 19:20:22 +1200

    gui displays empty and floor tiles

--- Program/src/nz.ac.vuw.ecs.swen225.gp20/render/BoardRender.java
+++ Program/src/nz.ac.vuw.ecs.swen225.gp20/render/BoardRender.java
@@ -1,22 +1,97 @@
 package nz.ac.vuw.ecs.swen225.gp20.render;
 
-import nz.ac.vuw.ecs.swen225.gp20.application.Main;
-
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.net.URL;
 
 public class BoardRender {
 
-    private static ImageIcon boardPartition = makeImageIcon("boardPartition.png");
+    private ImageIcon empty = makeImageIcon("/tiles/empty.png");
+    private ImageIcon floor = makeImageIcon("/tiles/floor.png");
+
+    JPanel chipGrid;
+    JFrame testFrame;
+
+    private static final int width = 9;
+    private static final int height = 9;
+
+    private JPanel outerMostPanel;
+    private JLabel[][] grid;
+
+    private String[][] board;
 
     public BoardRender() {
+        board = new String[height][width];
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                if (row % 2 == 0) {
+                    board[row][col] = "floor";
+                } else {
+                    board[row][col] = "empty";
+                }
+            }
+        }
+        this.testFrame = new JFrame();
 
+        makeOutermostPanel();
+        testFrame.getContentPane().add(outerMostPanel);
+        drawBoard();
+
+
+        testFrame.pack();
+
+        testFrame.setVisible(true);
     }
 
-    private static ImageIcon makeImageIcon(String filename) {
+    /**
+     * This method is used to create the outermost panel.
+     * @return
+     */
+    private void makeOutermostPanel() {
+        grid = new JLabel[width][height];
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                grid[x][y] = new JLabel(empty);
+            }
+        }
+
+        chipGrid = new JPanel(new GridLayout(height, width, 1, 1));
+        chipGrid.setBorder(new EmptyBorder(15, 15, 15, 15));
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                chipGrid.add(grid[row][col]);
+            }
+        }
+
+        outerMostPanel = new JPanel(new BorderLayout());
+        outerMostPanel.add(chipGrid, BorderLayout.CENTER);
+        drawBoard();
+    }
+
+    public void drawBoard() {
+        for (int row = 0; row < height; row ++) {
+            for (int col = 0; col < width; col ++) {
+                grid[row][col].setIcon(getSquareAt(row, col));
+            }
+        }
+    }
+
+    public ImageIcon getSquareAt(int row, int col) {
+        if (board[row][col] == "empty") {
+            return empty;
+        } else if (board[row][col] == "floor") {
+            return floor;
+        } else {
+            return null;
+        }
+    }
+
+    private ImageIcon makeImageIcon(String filename) {
         // using the URL means the image loads when stored
         // in a jar or expanded into individual files.
-        URL imageURL = Main.class.getResource(filename);
+        URL imageURL = this.getClass().getResource(filename);
 
         ImageIcon icon = null;
         if (imageURL != null) {
@@ -24,5 +99,7 @@ public class BoardRender {
         }
         return icon;
     }
+
+
 
 }
---
+++ Program/src/nz.ac.vuw.ecs.swen225.gp20/render/renderTest.java
@@ -0,0 +1,8 @@
+package nz.ac.vuw.ecs.swen225.gp20.render;
+
+public class renderTest {
+
+    public static void main(String[] args) {
+        new BoardRender();
+    }
+}
