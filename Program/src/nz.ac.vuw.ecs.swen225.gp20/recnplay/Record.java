2c8e3c89483b5d46be342f7670fc0f88bcc96e42
tyla turner <turnertyla@ecs.vuw.ac.nz>
2020-09-23 23:34:52 +1200

    Record Class Created

---
+++ Program/src/nz.ac.vuw.ecs.swen225.gp20/recnplay/Record.java
@@ -0,0 +1,11 @@
+package nz.ac.vuw.ecs.swen225.gp20.recnplay;
+
+/**
+ * This class is responsible for recording and replaying the gameplay.
+ * Each movement of Chap is recorded and saved to allow for a replay of a level.
+ *
+ * @author Tyla Turner - 300473374
+ */
+public class Record {
+
+}
