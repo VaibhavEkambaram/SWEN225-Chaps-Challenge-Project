de1d217572b7d39cee8d60291860209b6a02770e
tyla turner <turnertyla@ecs.vuw.ac.nz>
2020-09-25 16:42:45 +1200

    Finished Record(). Ready to be used within the persistence module.

--- Program/src/nz.ac.vuw.ecs.swen225.gp20/recnplay/Record.java
+++ Program/src/nz.ac.vuw.ecs.swen225.gp20/recnplay/Record.java
@@ -1,9 +1,13 @@
 package nz.ac.vuw.ecs.swen225.gp20.recnplay;
 
-
 import nz.ac.vuw.ecs.swen225.gp20.application.Game;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Tile;
+import nz.ac.vuw.ecs.swen225.gp20.persistence.readWrite;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import java.io.*;
 import java.util.ArrayList;
 
 /**
@@ -15,16 +19,96 @@ import java.util.ArrayList;
 public class Record {
 
     private static ArrayList<Tile.Directions> movements = new ArrayList<>();
+    private static ArrayList<Integer> actors = new ArrayList<>();
     private static String saveFile;
     private static String state;
 
     private static boolean isRecording;
     private static boolean isRunning;
 
+    static Thread thread;
+
     public static void newSave(Game g, String s){
-        
+        saveFile = s;
+        isRecording = true;
+        movements.clear();
+        state = readWrite.getGameState(g);
+    }
+
+    public static void addMoves(Tile.Directions d){
+        if (isRecording){
+            movements.add(d);
+            actors.add(0);
+        }
+    }
+
+    public static void saveRecording(Game g)  {
+        if (isRecording){
+            JsonArrayBuilder a = Json.createArrayBuilder();
+
+            for (int i = 0; i < actors.size(); i++){
+                JsonObjectBuilder o = Json.createObjectBuilder().add("count", actors.get(i)).add("movement", movements.get(i).toString());
+                a.add(o.build());
+            }
+
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder().add("game", state).add("movements", a).add("timeLeft", g.getTimeLeft());
+
+            //Attempt to save the moves to a JSON file
+            try(Writer writer = new StringWriter()){
+                Json.createWriter(writer).write(objectBuilder.build());
+                try {
+                    BufferedWriter buffWriter = new BufferedWriter(new FileWriter(saveFile));
+                    buffWriter.write(writer.toString());
+                    buffWriter.close();
+                }
+                catch (IOException e){
+                    throw new Error("Movements were unable to save." + e);
+                }
+            }
+            catch(IOException e){
+                throw new Error("Movements were unable to save." + e);
+            }
+            isRecording = false;
+        }
+    }
+
+    public static void endRecording(){
+        isRecording = false;
+        isRunning = false;
+        saveFile = null;
+        movements.clear();
+        actors.clear();
+        state = null;
+        thread = null;
+    }
+
+    public static void storeActorMove(Tile.Directions d, int id){
+        if(isRecording){
+            movements.add(d);
+            actors.add(id);
+        }
     }
     public static boolean getIsRunning(){
         return isRunning;
+    }
+
+    public static ArrayList<Tile.Directions> getMovements() {
+        return movements;
+    }
+
+    public static ArrayList<Integer> getActors() {
+        return actors;
+    }
+
+    public static String getSaveFile() {
+        return saveFile;
+    }
+
+    public static String getState() {
+        return state;
+    }
+
+    public static Thread getThread() {
+        return thread;
     }
 }
