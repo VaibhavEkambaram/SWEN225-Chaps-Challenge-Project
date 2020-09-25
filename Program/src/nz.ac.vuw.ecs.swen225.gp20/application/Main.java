6f4b37f8646845050d2ad3b770ddd184183c6f86
Vaibhav Ekambaram <ekambavaib@ecs.vuw.ac.nz>
2020-09-25 23:27:19 +1200

    updated package naming convention

--- Program/src/nz.ac.vuw.ecs.swen225.gp20/application/Main.java
+++
@@ -1,8 +0,0 @@
-package nz.ac.vuw.ecs.swen225.gp20.application;
-
-public class Main {
-    public static void main(String[] args){
-        // for testing purposes
-        new GraphicalInterface();
-    }
-}
--- Program/src/nz.ac.vuw.ecs.swen225.gp20/monkey/Test1.java
+++
@@ -1,4 +0,0 @@
-package nz.ac.vuw.ecs.swen225.gp20.monkey;
-
-public class Test1 {
-}
