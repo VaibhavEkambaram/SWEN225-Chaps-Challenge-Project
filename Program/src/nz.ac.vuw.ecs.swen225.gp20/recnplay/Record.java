265c936d12a2169393f96fbd61512b6433b178ed
tyla turner <turnertyla@ecs.vuw.ac.nz>
2020-09-25 15:14:01 +1200

    Initialised Record Fields (testing push/pull)

--- Program/src/nz.ac.vuw.ecs.swen225.gp20/recnplay/Record.java
+++ Program/src/nz.ac.vuw.ecs.swen225.gp20/recnplay/Record.java
@@ -1,5 +1,11 @@
 package nz.ac.vuw.ecs.swen225.gp20.recnplay;
 
+
+import nz.ac.vuw.ecs.swen225.gp20.application.Game;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Tile;
+
+import java.util.ArrayList;
+
 /**
  * This class is responsible for recording and replaying the gameplay.
  * Each movement of Chap is recorded and saved to allow for a replay of a level.
@@ -8,4 +14,17 @@ package nz.ac.vuw.ecs.swen225.gp20.recnplay;
  */
 public class Record {
 
+    private static ArrayList<Tile.Directions> movements = new ArrayList<>();
+    private static String saveFile;
+    private static String state;
+
+    private static boolean isRecording;
+    private static boolean isRunning;
+
+    public static void newSave(Game g, String s){
+        
+    }
+    public static boolean getIsRunning(){
+        return isRunning;
+    }
 }
