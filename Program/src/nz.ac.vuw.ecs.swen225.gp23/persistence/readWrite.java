f09d4903b713fc98b42442e3452ddf3a9d5e56bf
Rahul Mahasuriya <mahasurahu@ecs.vuw.ac.nz>
2020-09-27 18:45:37 +1300

    Donezo with the first level, also updated some of the read/write class. Beginning work with a level manager for accessing the levels, also a loading method for json files in the persistence

--- Program/src/nz.ac.vuw.ecs.swen225.gp23/persistence/readWrite.java
+++ Program/src/nz.ac.vuw.ecs.swen225.gp23/persistence/readWrite.java
@@ -8,10 +8,7 @@ import nz.ac.vuw.ecs.swen225.gp23.maze.Tile;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
-import javax.json.JsonPatch;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+import java.io.*;
 
 public class readWrite {
 
@@ -23,12 +20,14 @@ public class readWrite {
         Board b = game.getBoard();
         JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
 
-        for(Tile t : ){}
+        for(Tile[] t : b.getTilesXY()){
+         //   arrayBuilder.add(t.getJson());
+        }
 
-        JsonObjectBuilder builder = Json.createObjectBuilder().add("boardSize", );
+        JsonObjectBuilder builder = Json.createObjectBuilder().add("boardSize", b.getBoardDimension()).add("allTiles", arrayBuilder);
 
         try (Writer w = new StringWriter()){
-            Json.createWriter(w).write();
+            Json.createWriter(w).write(builder.build());
             Board = w.toString();
 
         } catch (IOException e){
@@ -38,17 +37,63 @@ public class readWrite {
         Player p = game.getPlayer();
         arrayBuilder = Json.createArrayBuilder();
 
+        for(String l : p.getInventory()){
+            arrayBuilder.add(l);
+        }
+
+     //   builder = Json.createObjectBuilder().add("location", p.getCurrentTile().getJson())
+          //      .add("inventory", arrayBuilder);
+                //.add("treasures", p.getTreasures());
+
+        try(Writer w = new StringWriter()){
+            Json.createWriter(w).write(builder.build());
+            Player = w.toString();
+        } catch (IOException e){
+            throw new Error("Failed");
+        }
+
+        arrayBuilder = Json.createArrayBuilder();
+
+  /*      builder = Json.createObjectBuilder().add("level", game.getLevel())
+                .add("timeLeft", game.getTimeLeft())
+                .add("board", Board)
+                .add("player", Player)
+                .add("mobs", arrayBuilder);*/
+
+        try(Writer w = new StringWriter()){
+            Json.createWriter(w).write(builder.build());
+            Game = w.toString();
+        } catch (IOException e){
+            throw new Error("Failed");
+        }
+        return Game;
     }
 
     public static void saveState(Game game, String n){
 
     }
 
-    public static Game loadStateFromJsonFIle(String fn, Game game){
+    public static Game loadStateFromJsonFIle(String fn, Game game)
+        throws IOException{
+            InputStream r = new FileInputStream(new File(fn));
+            InputStreamReader iR = new InputStreamReader(r);
+            BufferedReader bR = new BufferedReader(iR);
+            String line = bR.readLine();
+            if(line == null){
+                line = "";
+            }
+            Game g = loadStateFromJsonFIle(line, game);
+            iR.close();
+            bR.close();
+            return g;
+
+    }
+
+  /*  public static Game loadState(String save, Game game){
 
     }
 
     public static Tile createTile(String t){
 
-    }
+    }*/
 }
