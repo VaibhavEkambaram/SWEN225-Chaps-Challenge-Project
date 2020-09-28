fde9988ef258610693243a0e52a99967a878bcc9
Rahul Mahasuriya <mahasurahu@ecs.vuw.ac.nz>
2020-09-28 15:48:37 +1300

    renamed level 1

--- Program/src/nz.ac.vuw.ecs.swen225.gp23/persistence/levelM.java
+++ Program/src/nz.ac.vuw.ecs.swen225.gp23/persistence/levelM.java
@@ -1,4 +1,16 @@
 package nz.ac.vuw.ecs.swen225.gp23.persistence;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class levelM {
+    private static ArrayList<String> lDesc = new ArrayList<>();
+    public static int currentLevel = 0;
+    @SuppressWarnings("rawtypes")
+    static Set<Class> cSet = new HashSet<>();
+
+    private static final String pathForAsset = "Program/src/nz.ac.vuw.ecs.swen225.gp23/render/tile_images"
+
+    public static void loadLevel(assetManager
 }
