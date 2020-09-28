9b1fbc5444a0afcdf1ed7ae6f68267119f92a9fe
sushil shamra <sharmasush@myvuw.ac.nz>
2020-09-28 19:00:06 +1300

    Creating some tests for coverage of maze and persistence

---
+++ Program/src/nz.ac.vuw.ecs.swen225.gp23/monkey/MazeTests.java
@@ -0,0 +1,92 @@
+package nz.ac.vuw.ecs.swen225.gp23.monkey;
+import nz.ac.vuw.ecs.swen225.gp23.maze.Key;
+import nz.ac.vuw.ecs.swen225.gp23.maze.LockedDoor;
+import org.junit.Test;
+
+import static junit.framework.TestCase.*;
+
+/**
+ * @author Sushil Sharma
+ */
+public class MazeTests {
+
+    /**
+     * Checking if the key is blue.
+     */
+    @Test
+    public void blueKey(){
+        Key key = new Key("Blue");
+        String keyCol = key.toString();
+        assertEquals(keyCol, "key_Blue");
+    }
+
+    /**
+     * Checking if the key is red.
+     */
+    @Test
+    public void redKey(){
+        Key key = new Key("Red");
+        String keyCol = key.toString();
+        assertEquals(keyCol, "key_Red");
+    }
+
+    /**
+     * Checking if the key is green.
+     */
+    @Test
+    public void greenKey(){
+        Key key = new Key("Green");
+        String keyCol = key.toString();
+        assertEquals(keyCol, "key_Green");
+    }
+
+    /**
+     * Checking if the key is yellow.
+     */
+    @Test
+    public void yellowKey(){
+        Key key = new Key("Yellow");
+        String keyCol = key.toString();
+        assertEquals(keyCol, "key_Yellow");
+    }
+
+    /**
+     * Checking if the locked door is blue.
+     */
+    @Test
+    public void lockedBlueDoor(){
+        LockedDoor ld = new LockedDoor("Blue");
+        String doorCol = ld.toString();
+        assertEquals(doorCol, "door_key_Blue");
+    }
+
+    /**
+     * Checking if the locked door is red.
+     */
+    @Test
+    public void lockedRedDoor(){
+        LockedDoor ld = new LockedDoor("Red");
+        String doorCol = ld.toString();
+        assertEquals(doorCol, "door_key_Red");
+    }
+
+    /**
+     * Checking if the locked door is green.
+     */
+    @Test
+    public void lockedGreenDoor(){
+        LockedDoor ld = new LockedDoor("Green");
+        String doorCol = ld.toString();
+        assertEquals(doorCol, "door_key_Green");
+    }
+
+    /**
+     * Checking if the locked door is yellow.
+     */
+    @Test
+    public void lockedYellowDoor(){
+        LockedDoor ld = new LockedDoor("Yellow");
+        String doorCol = ld.toString();
+        assertEquals(doorCol, "door_key_Yellow");
+    }
+}
---
+++ Program/src/nz.ac.vuw.ecs.swen225.gp23/monkey/PersistenceTests.java
@@ -0,0 +1,50 @@
+package nz.ac.vuw.ecs.swen225.gp23.monkey;
+
+import nz.ac.vuw.ecs.swen225.gp23.application.Application;
+import nz.ac.vuw.ecs.swen225.gp23.application.Game;
+import nz.ac.vuw.ecs.swen225.gp23.application.GraphicalInterface;
+import nz.ac.vuw.ecs.swen225.gp23.persistence.readWrite;
+import org.junit.Test;
+
+import javax.swing.*;
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * JUnit tests for checking all objects are add0ed correctly.
+ *
+ * @author Sushil Sharma
+ */
+public class PersistenceTests {
+    //private Game game = new Game();
+    private Application app = new Application();
+
+    private static boolean test = false;
+    private Game game;
+
+    /**
+     * Invalid game load.
+     */
+    @Test
+    public void startGameInvalid(){
+        boolean invalid = false;
+        try{
+            readWrite.loadStateFromJsonFIle("invalid.txt", game);
+        }catch (IOException e){
+            invalid = true;
+        }
+        assertTrue(invalid);
+    }
+
+    /**
+     *First time game starts up.
+     */
+    @Test
+    public void firstStartUp(){
+        GraphicalInterface gui = new GraphicalInterface(app);
+        gui.setVisible(true);
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        assert(true);
+    }
+}
