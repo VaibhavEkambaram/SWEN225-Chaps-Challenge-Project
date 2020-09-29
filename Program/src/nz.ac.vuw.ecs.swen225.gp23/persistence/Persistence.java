71c269b28bbe014d9a85592428bde9acda6a207b
Vaibhav Ekambaram <ekambavaib@ecs.vuw.ac.nz>
2020-09-29 16:08:02 +1300

    revert persistence merge error

--- Program/src/nz.ac.vuw.ecs.swen225.gp23/persistence/Persistence.java
+++ Program/src/nz.ac.vuw.ecs.swen225.gp23/persistence/Persistence.java
@@ -54,7 +54,7 @@ public class Persistence {
     }
 
     public Board readBoard(String maze) {
-        Board board = new Board(game,boardX,boardY);
+        Board board = new Board(game, boardX, boardY);
 
         Scanner sc = new Scanner(maze).useDelimiter(",");
         int xValue = 0;
@@ -63,24 +63,50 @@ public class Persistence {
         while (sc.hasNext()) {
 
             System.out.print("|");
-           // System.out.print(sc.next());
+            // System.out.print(sc.next());
 
             String value = sc.next();
             System.out.printf(value);
 
             switch (value) {
-
+                case "B":
+                    board.setTile(xValue, yValue, new LockedDoor("blue"));
+                    break;
+                case "G":
+                    board.setTile(xValue, yValue, new LockedDoor("green"));
+                    break;
+                case "R":
+                    board.setTile(xValue, yValue, new LockedDoor("red"));
+                    break;
+                case "Y":
+                    board.setTile(xValue, yValue, new LockedDoor("yellow"));
+                    break;
+                case "g":
+                    board.setTile(xValue, yValue, new Key("green"));
+                    break;
+                case "b":
+                    board.setTile(xValue, yValue, new Key("blue"));
+                    break;
+                case "y":
+                    board.setTile(xValue, yValue, new Key("yellow"));
+                    break;
+                case "r":
+                    board.setTile(xValue, yValue, new Key("red"));
+                    break;
                 case "_":
-                    board.setTile(xValue,yValue,new Floor());
+                    board.setTile(xValue,yValue,new Wall());
                     break;
                 case "#":
-                    board.setTile(xValue,yValue,new Wall());
+                    board.setTile(xValue,yValue,new Empty());
                     break;
                 case "i":
                     board.setTile(xValue, yValue, new Hint());
                     break;
                 case "T":
                     board.setTile(xValue, yValue, new ComputerChip());
+                    break;
+                case "l":
+                    board.setTile(xValue,yValue, new ExitLock());
                     break;
                 case "E":
                     board.setTile(xValue,yValue,new Exit());
@@ -101,5 +127,4 @@ public class Persistence {
         return board;
     }
 }
-
 
