e953c0bd775afad197e3b6b30e366d3a737d9c9d
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2020-10-12 11:58:30 +1300

    Sprite Removal

--- Program/src/nz.ac.vuw.ecs.swen225.gp23/render/TileFinder.java
+++ Program/src/nz.ac.vuw.ecs.swen225.gp23/render/TileFinder.java
@@ -27,27 +27,6 @@ public class TileFinder {
     private final ImageIcon chip_down = makeImageIcon("/chip/chip_down.png");
     private final ImageIcon chip_left = makeImageIcon("/chip/chip_left.png");
     private final ImageIcon chip_right= makeImageIcon("/chip/chip_right.png");
-    private final ImageIcon chip_swim_up = makeImageIcon("/chip/chip_swim_up.png");
-    private final ImageIcon chip_swim_down = makeImageIcon("/chip/chip_swim_down.png");
-    private final ImageIcon chip_swim_left = makeImageIcon("/chip/chip_swim_left.png");
-    private final ImageIcon chip_swim_right= makeImageIcon("/chip/chip_swim_right.png");
-
-    // Bug
-    private final ImageIcon bug_up = makeImageIcon("/monsters/bug/bug_up.png");
-    private final ImageIcon bug_down = makeImageIcon("/monsters/bug/bug_down.png");
-    private final ImageIcon bug_left = makeImageIcon("/monsters/bug/bug_left.png");
-    private final ImageIcon bug_right= makeImageIcon("/monsters/bug/bug_right.png");
-
-    // Tank
-    private final ImageIcon tank_up = makeImageIcon("/monsters/tank/tank_up.png");
-    private final ImageIcon tank_down = makeImageIcon("/monsters/tank/tank_down.png");
-    private final ImageIcon tank_left = makeImageIcon("/monsters/tank/tank_left.png");
-    private final ImageIcon tank_right= makeImageIcon("/monsters/tank/tank_right.png");
-    // Glider
-    private final ImageIcon glider_up = makeImageIcon("/monsters/glider/glider_up.png");
-    private final ImageIcon glider_down = makeImageIcon("/monsters/glider/glider_down.png");
-    private final ImageIcon glider_left = makeImageIcon("/monsters/glider/glider_left.png");
-    private final ImageIcon glider_right= makeImageIcon("/monsters/glider/glider_right.png");
 
     // Keys
     private final ImageIcon key_green = makeImageIcon("/items/keys/key_green.png");
@@ -79,13 +58,7 @@ public class TileFinder {
             return getChip(tileName);
         } else if (tileName.startsWith("key")) {
             return getKey(tileName);
-        } else if (tileName.startsWith("tank")) {
-            return getTank(tileName);
-        } else if (tileName.startsWith("bug")) {
-            return getBug(tileName);
-        } else if (tileName.startsWith("glider")) {
-            return getGlider(tileName);
-        } else if (tileName.startsWith("door")) {
+        }else if (tileName.startsWith("door")) {
             return getDoor(tileName);
         }
 
@@ -156,14 +129,6 @@ public class TileFinder {
                 return chip_left;
             case "chip_right":
                 return chip_right;
-            case "chip_swim_up":
-                return chip_swim_up;
-            case "chip_swim_down":
-                return chip_swim_down;
-            case "chip_swim_left":
-                return chip_swim_left;
-            case "chip_swim_right":
-                return chip_swim_right;
             default:
                 throw new Error("TileFinder getChip() - No such chip as: " + tileName);
         }
@@ -207,84 +172,6 @@ public class TileFinder {
                 return key_red;
             default:
                 throw new Error("TileFinder getKey() - No such key as: " + tileName);
-        }
-    }
-
-    /**
-     * Given a String that starts with tank.
-     * Search for the specific tank tile.
-     *
-     * @param tileName
-     * Name of tank tile
-     * @return
-     * Requested tank Tile as an ImageIcon.
-     *
-     * @author Cameron Li
-     */
-    private ImageIcon getTank(String tileName) {
-        switch(tileName) {
-            case "tank_up":
-                return tank_up;
-            case "tank_down":
-                return tank_down;
-            case "tank_left":
-                return tank_left;
-            case "tank_right":
-                return tank_right;
-            default:
-                throw new Error("TileFinder getTank() - No such Tank as: " + tileName);
-        }
-    }
-
-    /**
-     * Given a String that starts with bug.
-     * Search for the specific bug tile.
-     *
-     * @param tileName
-     * Name of bug tile
-     * @return
-     * Requested bug Tile as an ImageIcon.
-     *
-     * @author Cameron Li
-     */
-    private ImageIcon getBug(String tileName) {
-        switch(tileName) {
-            case "bug_up":
-                return bug_up;
-            case "bug_down":
-                return bug_down;
-            case "bug_left":
-                return bug_left;
-            case "bug_right":
-                return bug_right;
-            default:
-                throw new Error("TileFinder getBug() - No such Bug as: " + tileName);
-        }
-    }
-
-    /**
-     * Given a String that starts with glider.
-     * Search for the specific glider tile.
-     *
-     * @param tileName
-     * Name of glider tile
-     * @return
-     * Requested glider Tile as an ImageIcon.
-     *
-     * @author Cameron Li
-     */
-    private ImageIcon getGlider(String tileName) {
-        switch(tileName) {
-            case "glider_up":
-                return glider_up;
-            case "glider_down":
-                return glider_down;
-            case "glider_left":
-                return glider_left;
-            case "glider_right":
-                return glider_right;
-            default:
-                throw new Error("TileFinder getGlider() - No such Glider as: " + tileName);
         }
     }
 }
