c0516e5297adc1bd8d2d2a0c23d04ed9125981b1
Rahul Mahasuriya <mahasurahu@ecs.vuw.ac.nz>
2020-10-20 22:28:48 +1300

    New levelM class which will manage the current level state, getters and setters

--- Program/src/nz.ac.vuw.ecs.swen225.gp23/persistence/LevelM.java
+++ Program/src/nz.ac.vuw.ecs.swen225.gp23/persistence/LevelM.java
@@ -1,30 +1,39 @@
 package nz.ac.vuw.ecs.swen225.gp23.persistence;
+
+import com.google.common.io.Files;
+import com.google.gson.Gson;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
 import java.io.*;
 
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 import java.util.*;
 
 
 public class LevelM {
-    public static int currentLevel = 0;
-
-    static Set<Class> cSet = new HashSet<>();
 
 
+    HashMap<Integer, String> levels = new HashMap<>();
+    HashMap<String, Integer> currentLevelInteger = new HashMap<>();
+    int currentLevel = 1;
 
-    public static void loadLevel(){
-        File[] folder = new File("src/levels/").listFiles();
-        List<File> files = new ArrayList<>();
-        if(folder == null){
-            return;
-        }
-        for(File f : folder){
-            files.add(f);
-        }
 
+    public void setLevel(int value) { currentLevel = value; }
+
+
+    public void incrementLevel() {
+        currentLevel++;
     }
 
-    public static int getIntOfCurrentLevel(){
-        return currentLevel;
+    public String getCurrentLevel() {
+        return levels.get(currentLevel);
     }
+
+
+
+
+
 
 }
