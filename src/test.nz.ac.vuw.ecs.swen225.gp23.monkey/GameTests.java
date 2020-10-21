2203f8d3391acba5a2bbae753a588d6546f8dd2a
sushil shamra <sharmasush@myvuw.ac.nz>
2020-10-21 19:38:30 +1300

    Created some tests for the test cases

--- src/test.nz.ac.vuw.ecs.swen225.gp23.monkey/GameTests.java
+++ src/test.nz.ac.vuw.ecs.swen225.gp23.monkey/GameTests.java
@@ -81,17 +81,28 @@ public class GameTests {
     }
 
     /**
-     * Checking to see if the game pauses and un-pauses correctly.
+     * Check that the game pauses when running.
      */
     @Test
-    public void pauseGame(){
+    public void checkGamePaused(){
+        gui.onLoadGameNoGui("level1.json", false);
         Game game = gui.getCurrentGame();
-        assertFalse(game.isPaused());
-        game.setGamePaused(true);
 
-      //  assertTrue(game.isPaused());
-       // game.setGamePaused(false);
-        //assertTrue(game.isPaused());
+        game.isRunningTest(true);
+        gui.onPauseGame(true);
+        assertTrue(game.isPaused());
+    }
+
+    /**
+     * Check that the game is not paused when first running.
+     */
+    @Test
+    public void checkGameNotPaused(){
+        gui.onLoadGameNoGui("level1.json", false);
+        Game game = gui.getCurrentGame();
+
+        game.isRunningTest(true);
+        assertFalse(game.isPaused());
     }
 
 
